2120316c03e8a2992841312af6376cfb758e7e76
GokceKahraman <gokcekahraman0@gmail.com>
2022-12-07 11:46:21 +0300

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Gökçe Kahraman on 7.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
